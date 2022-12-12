deadde419bee207ceff7845a51cd021fe186ad90
Rock <1598007611@qq.com>
2022-12-12 21:45:26 +0800

    Create2

--- examples/DeployWithCreate2.sol
+++ examples/DeployWithCreate2.sol
@@ -1,51 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract Pair {
+contract DeployWithCreate2 {
 
-    address public factory;
-    address public token0;
-    address public token1;
-
-    constructor() payable {
-        factory = msg.sender;
-    }
-
-    function initialize(address _token0, address _token1) external {
-        require(msg.sender == factory, "Not Auth");
-        token0 = _token0;
-        token1 = _token1;
+    address public owner;
+    constructor(address _owner) {
+        owner = _owner;
     }
 }
 
+contract Create2Factory {
 
-contract PairFactory {
+    event Deploy(address addr);
 
-    mapping(address => mapping(address => address)) public getPair;
-    address[] public allPairs;
-
-    function createPair(address tokenA, address tokenB) external returns (address pairAddr) {
-        require(tokenA != tokenB, 'IDENTICAL_ADDRESSES'); 
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        Pair pair = new Pair{salt: salt}();
-        pair.initialize(tokenA, tokenB);
-        pairAddr = address(pair);
-        allPairs.push(pairAddr);
-        getPair[tokenA][tokenB] = pairAddr;
-        getPair[tokenB][tokenA] = pairAddr;
+    function deploy(uint _salt) external {
+        DeployWithCreate2 _contract = new DeployWithCreate2{salt: bytes32(_salt)}(msg.sender);
+        emit Deploy(address(_contract));
     }
 
-    function calculateAddr(address tokenA, address tokenB) public view returns(address predictedAddress){
-        require(tokenA != tokenB, 'IDENTICAL_ADDRESSES');
-        // 计算用tokenA和tokenB地址计算salt
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        predictedAddress = address(uint160(uint(keccak256(abi.encodePacked(
-            bytes1(0xff),
-            address(this),
-            salt,
-            keccak256(type(Pair).creationCode)
-        )))));
+    function getAddress(uint _salt) public view returns(address) {
+        bytes memory bytecode = abi.encodePacked(type(DeployWithCreate2).creationCode, abi.encode(msg.sender));
+        bytes32 hash = keccak256(
+                abi.encodePacked(
+                    bytes1(0xff), 
+                    address(this), 
+                    _salt, 
+                    keccak256(bytecode)
+            )
+        );
+        return address(uint160(uint(hash)));
     }
+   
 }
